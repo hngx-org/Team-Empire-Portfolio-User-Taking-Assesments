f77b0774f57b044cda6ef3becb4d273c23eb266b
Neon-jeff <omaksteejay@gmail.com>
2023-10-16 00:41:53 +0100

    added endpoint for listing all user completed assessments

--- app/services/assessment.py
+++ app/services/assessment.py
@@ -29,7 +29,7 @@ def get_assessment_results(user_id: str, assessment_id: int, db : Session):
         status of the assessment
     - db_questions : List[Question]
         list of questions in the assessment
-        
+
     """
 
     valid_user = db.query(User).filter(User.id == user_id).first()
@@ -39,12 +39,12 @@ def get_assessment_results(user_id: str, assessment_id: int, db : Session):
             status_code=status.HTTP_404_NOT_FOUND,
             detail=f"User with id {user_id} not found",
         )
-    
+
     query = db.query(UserAssessment)\
         .filter(
             and_(UserAssessment.user_id==user_id, UserAssessment.assessment_id==assessment_id)\
             )
-    
+
     assessment = query.first()
 
     if not assessment:
@@ -52,14 +52,14 @@ def get_assessment_results(user_id: str, assessment_id: int, db : Session):
             status_code=status.HTTP_404_NOT_FOUND,
             detail=f"Assessment with id {assessment_id} not found",
         )
-    
+
     score = assessment.score
     assessment_status = assessment.status
 
     db_questions = db.query(Question).join(Answer, Question.id == Answer.question_id)\
                     .filter(Question.assessment_id == assessment_id).all()
-    
-    
+
+
     questions_with_answers = []
     for item in db_questions:
         var = {}
@@ -68,7 +68,7 @@ def get_assessment_results(user_id: str, assessment_id: int, db : Session):
 
         answer = question['answer'].__dict__
         var['answer_text'] = answer['correct_option']
-        
+
         questions_with_answers.append(var)
 
 
@@ -80,21 +80,21 @@ def get_assessment_results(user_id: str, assessment_id: int, db : Session):
     # assessment_obj = None
 
     # for assessment in UserAssessments:
-     
+
     #     if assessment['assessment_id'] == assessment_id:
     #         assessment_obj = assessment
     #     else:
     #         continue
-    
+
 
     # if assessment_obj is None:
     #     raise HTTPException(
     #         status_code=status.HTTP_404_NOT_FOUND,
     #         detail=f"Assessment with id {assessment_id} not found",
     #     )
-    
+
     # score = assessment_obj.get('score')
-    # assessment_status = assessment_obj.get('status')   
+    # assessment_status = assessment_obj.get('status')
 
     # db_questions = [question for question in Questions if question['assessment_id'] == assessment_id]
     # for question in db_questions:
@@ -106,3 +106,29 @@ def get_assessment_results(user_id: str, assessment_id: int, db : Session):
     #             continue
 
     return score, assessment_status, questions_with_answers
+
+def get_completed_assessments(user_id,db:Session):
+    completed_assessments=db.query(UserAssessment).filter(UserAssessment.user_id==user_id,UserAssessment.status=='complete').all()
+    if completed_assessments==[]:
+        return None,HTTPException(status_code=404,detail="no assessment found")
+
+    response=[]
+    for assessment in completed_assessments:
+        response.append(
+            {
+                "assessment_id":assessment.assessment_id,
+                "score":assessment.score,
+                "id": assessment.id,
+                "user_id": assessment.user_id,
+                "status": assessment.status,
+                "submission_date": assessment.submission_date,
+                "badge_id":assessment.user_badge[0].badge_id,
+                "badge_name":assessment.user_badge[0].skill_badge.name,
+                "skill_id":assessment.user_badge[0].skill_badge.skill_id
+
+
+            }
+        )
+    return response,None
+
+
